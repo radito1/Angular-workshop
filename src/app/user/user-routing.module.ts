1d2daadcff34ea60891f8ac51456b558818d649b
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-07-14 10:17:48 +0300

    add html and css to login and register page

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoaderComponent } from '../shared/loader/loader.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoaderComponent,
+    component: LoginComponent,
   },
   {
     path: 'register',
